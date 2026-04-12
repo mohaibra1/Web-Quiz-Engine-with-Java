04515f3a956d4bc2ac4a8add7c570ddd392260b7
mohaibra <mimohadinho@gmail.com>
2026-04-13 00:49:06 +0200

    Lots of quizzes 2/6

--- Web Quiz Engine with Java/task/src/engine/QuizController.java
+++ Web Quiz Engine with Java/task/src/engine/QuizController.java
@@ -1,22 +1,57 @@
 package engine;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/quizzes")
 public class QuizController {
+    private final Quizzes quizzes;
 
-
-    @GetMapping("/api/quiz")
-    public Quiz quiz() {
-        return  new Quiz();
+    public QuizController(Quizzes quizzes) {
+        this.quizzes = quizzes;
     }
 
-    @PostMapping("/api/quiz")
-    public QuizResult solveQuiz(@RequestParam int answer) {
-        if(answer == 2) {
-            return new QuizResult(true,"Congratulations, you're right!");
-        }else {
-            return new QuizResult(false,"Wrong answer! Please, try again.");
+    // CREATE QUIZ
+    @PostMapping
+    public ResponseEntity<Quiz> addQuiz(@RequestBody Quiz quiz) {
+        Quiz saved =  quizzes.addQuiz(quiz);
+        return new ResponseEntity<>(saved, HttpStatus.OK);
+    }
+
+    // GET ALL QUIZZES
+    @GetMapping
+    public ResponseEntity<List<Quiz>> getAllQuizzes() {
+        return ResponseEntity.ok(quizzes.getQuizzes());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Quiz> getById(@PathVariable int id) {
+        Quiz quiz = quizzes.findById(id);
+
+        if(quiz  == null) {
+            throw new ResourcesNotFoundException("User not found");
         }
+        return ResponseEntity.ok(quiz);
+    }
+
+
+
+    @PostMapping("/{id}/solve")
+    public ResponseEntity<QuizResult> solveQuiz(@PathVariable int id, @RequestParam int answer) {
+        Quiz quiz = quizzes.findById(id);
+
+        if(quiz == null) {
+            throw new ResourcesNotFoundException("Not Found");
+        }
+
+        if(quiz.getAnswer() != null && quiz.getAnswer() == answer) {
+            return ResponseEntity.ok(new QuizResult(true,"Congratulations, you're right!"));
+        }
+
+        return ResponseEntity.ok(new QuizResult(false,"Wrong answer! Please, try again."));
     }
 }
---
+++ Web Quiz Engine with Java/task/src/engine/ResourcesNotFoundException.java
@@ -0,0 +1,7 @@
+package engine;
+
+public class ResourcesNotFoundException extends RuntimeException {
+    public ResourcesNotFoundException(String message) {
+        super(message);
+    }
+}
